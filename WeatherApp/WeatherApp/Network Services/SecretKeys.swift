890a23330382cd14571761f2b1bad7306da98bbe
alysonabril <alysonabril@pursuit.org>
2019-01-29 15:36:26 -0500

    added secret keys

---
+++ WeatherApp/WeatherApp/Network Services/SecretKeys.swift
@@ -0,0 +1,17 @@
+//
+//  SecretKeys.swift
+//  WeatherApp
+//
+//  Created by Alyson Abril on 1/29/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+struct SecretKeys {
+    static let aerisId = "Jg1U2qefaUUxRkaE9haYo"
+    static let aerisSecret = "sLCBTH120Tn4t6vCFROnjzp28z6nYGFOiCGrUixI"
+    static let pixabayKey = "11440520-006a635f4a58464f418d37ab2"
+}
+
+
